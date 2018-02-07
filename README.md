db8f9c73d8f9a917ef79e7d9d486cdd915e3aa58
Dayeol Lee <dayeol@berkeley.edu>
2018-02-07 06:56:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Introduction
+
+This Telegram Bot eavesdrop any chats and detects some keywords.
+You can subscribe or unsubscribe from the detection alert, and also add/remove the keywords.
+
+## Installation
+
+This bot depends on telepot. 
+
+```
+sudo pip install telepot
+```
+
+You should set your API Token in the ".telegram\_bot\_secret" file.
+
+## Execution
+
+Execute this at your server
+```
+python telepot.py 
+```
+
+## Usage
+
+/subscribe 		: Subscribe the chat room in which you're typing this command. If you subscribe, the bot will send an alert whenever it detects any keywords.
+
+/unsubscribe	: Unsubscribe from the bot alert
+
+/list					: List the keywords
+
+/add <keyword> : Add keyword
+
+/remove <keyword> : Remove the keyword
---
+++ telebot.py
@@ -0,0 +1,91 @@
+# telepot API: https://github.com/nickoala/telepot
+import time
+import re
+import random
+import requests
+import telepot
+from sets import Set
+from pprint import pprint
+from telepot.loop import MessageLoop
+
+forwardChat = Set([])
+keywords = Set([])
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print('Chat:', content_type, chat_type, chat_id)
+    chat_id, message_id = telepot.message_identifier(msg)   
+    # text behavior
+    try:
+        detected = False
+        for key in keywords:
+            if re.search(key, msg["text"], re.IGNORECASE):
+                detected = True
+
+        if detected:
+            for id in forwardChat:
+                print("forward to "+str(id))
+                print(bot.getChat(chat_id))
+                try:
+                    bot.sendMessage(id, "A keyword was detected at '"+bot.getChat(chat_id)["title"]+"'")
+                except KeyError, e:
+                    bot.sendMessage(id, bot.getChat(chat_id)["username"]+" mentioned a keyword")
+                print(message_id)
+                bot.forwardMessage(chat_id=id, from_chat_id=chat_id, message_id=message_id)
+
+        if "/subscribe" in msg["text"]:
+            forwardChat.add( chat_id )
+            print("subscribed: "+str(chat_id))
+            bot.sendMessage(chat_id, "You're subscribed to the bot")
+
+        if "/unsubscribe" in msg["text"]:
+            forwardChat.remove( chat_id )
+            print("unsubscribed: "+str(chat_id))
+            bot.sendMessage(chat_id, "You're unsubscribed from the bot")
+        
+        if "/list" in msg["text"]:
+            s = "Keywords: "
+            for key in keywords:
+                s += key+", "
+            bot.sendMessage(chat_id, s)
+
+        if "/add" in msg["text"]:
+            newKeys = msg["text"][4:].split(" ")
+            for key in newKeys:
+                if key != "":
+                    keywords.add(key.lower())
+            s = "Keywords: "
+            for key in keywords:
+                s += key+", "
+            bot.sendMessage(chat_id, s)
+        
+        if "/remove" in msg["text"]:
+            keywords.remove( msg["text"][7:].replace(" ",""))
+            s = "Keywords: "
+            for key in keywords:
+                s += key+", "
+            bot.sendMessage(chat_id, s)
+
+    except KeyError, e:
+        print("Recieved KeyError: %s" % e)
+
+def get_token(secret_path):
+    with open(secret_path) as f:
+        return f.read().strip()
+
+def main():
+    print("Bot starting")
+
+    MessageLoop(bot, {'chat': on_chat_message}).run_as_thread()
+        #'callback_query': on_callback_query,
+        #'inline_query': on_inline_query,
+        #'chosen_inline_result': on_chosen_inline_result}).run_as_thread()
+
+    #bot.message_loop()
+    while True:
+        time.sleep(10)
+
+bot = telepot.Bot(get_token(".telegram_bot_secret"))
+
+if __name__ == "__main__":
+    main()
